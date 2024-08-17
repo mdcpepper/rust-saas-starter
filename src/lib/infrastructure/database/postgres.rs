7d3b096d4571f51bcfb2df430afd9efdf0f7f90f
Mike Pepper <mdcpepper@gmail.com>
2024-08-17 14:19:16 +0100

    Add PostgresDatabaseError

--- src/lib/infrastructure/database/postgres.rs
+++ src/lib/infrastructure/database/postgres.rs
@@ -1,8 +1,26 @@
 //! Postgres module
 
-use anyhow::Result;
 use clap::Parser;
 use sqlx::PgPool;
+use thiserror::Error;
+
+use PostgresDatabaseError::*;
+
+/// Postgres database error
+#[derive(Debug, Error)]
+pub enum PostgresDatabaseError {
+    /// Empty connection string
+    #[error("Empty connection string")]
+    EmptyConnectionString,
+
+    /// Invalid connection string
+    #[error("Invalid connection string")]
+    InvalidConnectionString,
+
+    /// Connection error
+    #[error("Connection error: {0}")]
+    ConnectionError(sqlx::Error),
+}
 
 /// Database connection
 #[derive(Debug, Clone)]
@@ -13,9 +31,19 @@ pub struct PostgresDatabase {
 
 impl PostgresDatabase {
     /// Create a new database connection
-    pub async fn new(connection_string: &str) -> Result<Self> {
+    pub async fn new(connection_string: &str) -> Result<Self, PostgresDatabaseError> {
+        if connection_string.is_empty() {
+            return Err(EmptyConnectionString);
+        }
+
+        if !connection_string.starts_with("postgres://") {
+            return Err(InvalidConnectionString);
+        }
+
         Ok(Self {
-            pool: PgPool::connect(connection_string).await?,
+            pool: PgPool::connect(connection_string)
+                .await
+                .map_err(|err| ConnectionError(err))?,
         })
     }
 
@@ -31,4 +59,27 @@ pub struct DatabaseConnectionDetails {
     /// The database connection string
     #[arg(long, env = "DATABASE_URL")]
     pub connection_string: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_blank_connection_string_returns_error() {
+        let result = PostgresDatabase::new("").await;
+        assert!(matches!(
+            result,
+            Err(PostgresDatabaseError::EmptyConnectionString)
+        ));
+    }
+
+    #[tokio::test]
+    async fn test_invalid_connection_string_returns_error() {
+        let result = PostgresDatabase::new("invalid").await;
+        assert!(matches!(
+            result,
+            Err(PostgresDatabaseError::InvalidConnectionString)
+        ));
+    }
 }
