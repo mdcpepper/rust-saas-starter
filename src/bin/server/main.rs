ddb04921d8dd99db992b891ca1a33dcb78efd77a
Mike Pepper <mdcpepper@gmail.com>
2024-08-19 03:21:01 +0100

    Remove duplication

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -17,7 +17,7 @@ use anyhow::Result;
 use chrono::Utc;
 use clap::Parser;
 use rust_saas_starter::{
-    domain::auth::services::user::{UserManagement, UserService},
+    domain::auth::services::user::UserService,
     infrastructure::{
         database::postgres::{DatabaseConnectionDetails, PostgresDatabase},
         http::{
@@ -55,67 +55,52 @@ async fn main() -> Result<()> {
 
     let postgres = Arc::new(PostgresDatabase::new(&args.db.connection_string).await?);
 
-    let (ipv4_http_server, ipv6_http_server) =
-        get_http_servers(args.server.http_port, &args.server.base_url).await?;
-
     let state = AppState {
         start_time: Utc::now(),
         users: Arc::new(UserService::new(postgres)),
     };
 
-    let (ipv4_https_server, ipv6_https_server) = get_https_servers(
-        args.server.https_port,
-        &args.server.cert_path,
-        &args.server.key_path,
-        state,
-    )
-    .await?;
+    let http_port = args.server.http_port;
+    let https_port = args.server.https_port;
 
     let _ = tokio::join!(
-        tokio::spawn(ipv4_http_server.run()),
-        tokio::spawn(ipv6_http_server.run()),
-        tokio::spawn(ipv4_https_server.run()),
-        tokio::spawn(ipv6_https_server.run()),
+        tokio::spawn(
+            HttpServer::new(
+                SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), http_port),
+                &args.server.base_url,
+            )
+            .await?
+            .run()
+        ),
+        tokio::spawn(
+            HttpServer::new(
+                SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), http_port),
+                &args.server.base_url,
+            )
+            .await?
+            .run()
+        ),
+        tokio::spawn(
+            HttpsServer::new(
+                SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), https_port),
+                &args.server.cert_path,
+                &args.server.key_path,
+                state.clone(),
+            )
+            .await?
+            .run()
+        ),
+        tokio::spawn(
+            HttpsServer::new(
+                SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), https_port),
+                &args.server.cert_path,
+                &args.server.key_path,
+                state,
+            )
+            .await?
+            .run()
+        ),
     );
 
     Ok(())
-}
-
-async fn get_http_servers(http_port: u16, base_url: &str) -> Result<(HttpServer, HttpServer)> {
-    Ok((
-        HttpServer::new(
-            SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), http_port),
-            base_url,
-        )
-        .await?,
-        HttpServer::new(
-            SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), http_port),
-            base_url,
-        )
-        .await?,
-    ))
-}
-
-async fn get_https_servers(
-    port: u16,
-    cert_path: &str,
-    key_path: &str,
-    state: AppState<impl UserManagement>,
-) -> Result<(HttpsServer, HttpsServer)> {
-    Ok((
-        HttpsServer::new(
-            SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), port),
-            cert_path,
-            key_path,
-            state.clone(),
-        )
-        .await?,
-        HttpsServer::new(
-            SocketAddr::new(Ipv6Addr::UNSPECIFIED.into(), port),
-            cert_path,
-            key_path,
-            state,
-        )
-        .await?,
-    ))
 }
